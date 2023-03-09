ca415f19c9b9a77ae770f3349d3ff58c8edcdcd5
Henry Viloria <henrimavi123@gmail.com>
2023-03-09 06:32:28 -0500

    init commit

--- frontend/src/app/shared/spinner/spinner.component.ts
+++ frontend/src/app/shared/spinner/spinner.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.css']
 })
-export class SpinnerComponent {
+export class SpinnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
