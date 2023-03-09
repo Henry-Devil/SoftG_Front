f9dcaa2834ca40d79d72f4af5eb78602d5322df3
Henry Viloria <henrimavi123@gmail.com>
2023-03-09 06:31:13 -0500

    init commit

--- frontend/src/app/components/sign-in/sign-in.component.ts
+++ frontend/src/app/components/sign-in/sign-in.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
 
+  username: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
+  
+  addUser(){
+    //Validamos que el usuario ingrese valores
+    if(this.username == '' || this.password == '' || this.confirmPassword == ''){
+      alert('Todos los campos son obligatorios')
+    }
+  }
 }
