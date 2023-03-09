17424340e9182103a2ae72ae7067c9f275da04cf
Henry Viloria <henrimavi123@gmail.com>
2023-03-09 02:43:02 -0500

    ruteando login y redireccionando

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+//components
+import { LoginComponent } from './components/login/login.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'}
+  {path: 'login', component: LoginComponent},
+  {path: 'singIn', component: SignInComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
