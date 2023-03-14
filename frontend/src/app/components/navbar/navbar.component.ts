6090ba05f54802d48a976a2fd7de5be44ddb62ba
Henry Viloria <henrimavi123@gmail.com>
2023-03-14 11:39:32 -0500

    mejora del navbar

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+    isCollapsed = true; // Agregar la propiedad isCollapsed y definirla como true
+  }
 
-}
